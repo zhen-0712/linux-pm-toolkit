2e139da5d209c7466d1e71896898a5836ef5f646
zhen0712 <zhangyongzhen261@gmail.com>
2026-05-03 22:37:40 +0800

    feat(thermal): implement thermal_read with JSON and CSV formatters

--- src/monitor/thermal.c
+++ src/monitor/thermal.c
@@ -1,17 +1,31 @@
 #include "thermal.h"
+#include "sysfs_utils.h"
 
 #include <stdio.h>
 #include <string.h>
 
 int thermal_read(sysfs_reader_fn reader, int zone_id, thermal_zone_t *zone)
 {
-    /* TODO: implement in feature/userspace-thermal */
-    return -1;
+    char path[256];
+    memset(zone, 0, sizeof(*zone));
+    zone->zone_id = zone_id;
+
+    snprintf(path, sizeof(path),
+             "/sys/class/thermal/thermal_zone%d/temp", zone_id);
+    if (sysfs_read_long(reader, path, &zone->temp_milli_c) != 0)
+        return -1;
+
+    snprintf(path, sizeof(path),
+             "/sys/class/thermal/thermal_zone%d/type", zone_id);
+    sysfs_read_str(reader, path, zone->type, sizeof(zone->type));
+
+    return 0;
 }
 
 void thermal_print_json(const thermal_zone_t *zone)
 {
-    /* TODO: implement in feature/userspace-thermal */
+    printf("{\"zone\":%d,\"type\":\"%s\",\"temp_mc\":%ld}",
+           zone->zone_id, zone->type, zone->temp_milli_c);
 }
 
 void thermal_print_csv_header(void)
@@ -21,5 +35,6 @@ void thermal_print_csv_header(void)
 
 void thermal_print_csv(const thermal_zone_t *zone)
 {
-    /* TODO: implement in feature/userspace-thermal */
+    printf("%d,%s,%ld\n",
+           zone->zone_id, zone->type, zone->temp_milli_c);
 }
